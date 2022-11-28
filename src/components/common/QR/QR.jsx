a505a00d8c0031ac4d31ca65b841059f6e99fbb5
Victor Mendez <victor.mmc.pl@hotmail.com>
2022-11-27 21:28:59 -0600

    Animate QR component.

--- src/components/common/QR/QR.jsx
+++ src/components/common/QR/QR.jsx
@@ -1,15 +1,47 @@
 import './QR.scss'
 import React from 'react'
+import { motion } from 'framer-motion'
 
 function QR({ img, title, description }) {
-  return <section className='qr'>
-    <img src={img} alt="" />
+  return <motion.section
+  initial={{
+    opacity: 0
+  }}
+  animate={{
+    opacity: 1
+  }}
+  className='qr'
+  >
+    <motion.img
+    whileHover={{
+      y: 80,
+      scale: 1.5
+    }}
+    src={img} alt="" />
 
     <div className='qr-text'>
-        <h1>{title}</h1>
-        <p>{description}</p>
+        <motion.h1
+        initial={{
+          opacity: 0,
+          y: -100,
+        }}
+        animate={{
+          opacity: 1,
+          y: 0
+        }}
+        >{title}</motion.h1>
+        <motion.p
+        initial={{
+          opacity: 0,
+          y: -200,
+        }}
+        animate={{
+          opacity: 1,
+          y: 0
+        }}
+        >{description}</motion.p>
     </div>
-  </section>
+  </motion.section>
 }
 
 export default QR
