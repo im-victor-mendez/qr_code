235b1870603edf2b8c2634e08eb6b697f0bf16cd
Victor Mendez <victor.mmc.pl@hotmail.com>
2022-11-27 20:30:19 -0600

    Implemented QR component. TO VIEW A FIX OF NEW LINES.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import './App.css'
+import QR from './components/common/QR/QR'
+import qrImg from './assets/images/image-qr-code.png'
 
 function App() {
   return (
     <div className="App">
+      <QR
+      img={qrImg}
+
+      title={`Improve your front-end\n
+      skills by building projects`}
+
+      description={`Scan the QR code to visit Frontend\n
+      Mentor and take your coding skills to\n
+      the next level`}
+      />
     </div>
   )
 }
